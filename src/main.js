9936d9abf458d8bb95734451f0ad4b89a601c300
valtron <valerchik91@ya.ru>
2021-06-02 18:11:55 +0300

    basic dividing total sum between team. credit or debt info calculated

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import {createApp} from 'vue'
+import store from "core-js";
+import router from "./router";
+import App from "./App";
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).mount('#app');
